83146192eb16238cc4e5bdc74180144ca3d53c0c
Sarthak <sarthaktanwar50@gmail.com>
2023-09-25 00:13:04 +0530

    starter commit

---
+++ asset/_next/static/chunks/pages/index-f395cc2889a13a20.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5405],{4108:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(5654)}])},5654:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return e}});var t=_(5570),e=!0;u.default=t.default}},function(n){n.O(0,[1389,6737,5570,9774,2888,179],function(){return n(n.s=4108)}),_N_E=n.O()}]);
---
+++ asset/_next/static/cicn7QJp3sFY4L4q12BnK/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002F[...slug]","\u002Fcareers","\u002Fclients\u002F[slug]","\u002Fcsa","\u002Fenterprise","\u002Fhome","\u002Fpress","\u002Fpricing","\u002Fprivacy","\u002Fproduct","\u002Fresources","\u002Fsavings","\u002Fservice-terms","\u002Fterms"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
